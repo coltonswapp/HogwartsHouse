14d3b0c3d53932321a76f4a0abf0d5d077464070
Colton Swapp <coltonbswapp@gmail.com>
2020-07-30 18:23:26 -0600

    Built the CoreData basics

---
+++ Hogwartshouses/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Hogwartshouses
+//
+//  Created by Colton Swapp on 7/30/20.
+//  Copyright © 2020 Colton Swapp. All rights reserved.
+//
+
+import Foundation
---
+++ Hogwartshouses/HogwartsHousesCD + Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  HogwartsHousesCD + Convenience.swift
+//  Hogwartshouses
+//
+//  Created by Colton Swapp on 7/30/20.
+//  Copyright © 2020 Colton Swapp. All rights reserved.
+//
+
+import Foundation
