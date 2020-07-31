f9a69531582cd80353f71d1bec68e138cbbb6f78
Colton Swapp <coltonbswapp@gmail.com>
2020-07-30 18:05:23 -0600

    first commit

---
+++ Hogwartshouses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hogwartshouses
+//
+//  Created by Colton Swapp on 7/30/20.
+//  Copyright © 2020 Colton Swapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
